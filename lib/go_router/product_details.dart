7603b92910dfcef26e1ad0de4b41354fe26e6ccb
Atuoha Anthony <atuohainitiatives@gmail.com>
2023-12-28 12:34:03 +0100

    bottom sheet addon

--- lib/go_router/product_details.dart
+++ lib/go_router/product_details.dart
@@ -14,6 +14,8 @@ class ProductDetails extends StatefulWidget {
 }
 
 class _ProductDetailsState extends State<ProductDetails> {
+  void payNow() {}
+
   @override
   Widget build(BuildContext context) {
     var data =
@@ -23,6 +25,7 @@ class _ProductDetailsState extends State<ProductDetails> {
     List<String> availableColors = productDetails.colors.split(',');
     late Color colored;
 
+    // get color
     Color getColor(String color) {
       switch (color) {
         case 'red':
@@ -61,6 +64,7 @@ class _ProductDetailsState extends State<ProductDetails> {
       return colored;
     }
 
+    // show modal for image
     showImageModal(context) {
       showDialog(
         context: context,
@@ -113,6 +117,7 @@ class _ProductDetailsState extends State<ProductDetails> {
       );
     }
 
+    // build container for color
     Widget buildContainer(color) {
       return Container(
         height: 5,
@@ -258,6 +263,105 @@ class _ProductDetailsState extends State<ProductDetails> {
             ),
           ),
         ],
+      ),
+      bottomSheet: bottomContainer(productDetails),
+    );
+  }
+
+  // bottom container
+  Container bottomContainer(Product productDetails) {
+    return Container(
+      color: Colors.white,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(
+          horizontal: 18.0,
+          vertical: 10,
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Column(
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Text(
+                  'Price',
+                  style: TextStyle(
+                    color: Colors.grey,
+                    fontWeight: FontWeight.w500,
+                    fontSize: 14,
+                  ),
+                ),
+                const SizedBox(height: 5),
+                Text(
+                  '\$${productDetails.price}',
+                  style: const TextStyle(
+                    color: Colors.brown,
+                    fontWeight: FontWeight.w700,
+                    fontSize: 25,
+                  ),
+                )
+              ],
+            ),
+            Wrap(
+              crossAxisAlignment: WrapCrossAlignment.center,
+              children: [
+                Container(
+                  height: 50,
+                  width: 80,
+                  decoration: BoxDecoration(
+                    color: Colors.brown.withOpacity(0.3),
+                    borderRadius: const BorderRadius.only(
+                      bottomLeft: Radius.circular(5),
+                      topLeft: Radius.circular(5),
+                    ),
+                  ),
+                  child: const Center(
+                    child: Wrap(
+                      crossAxisAlignment: WrapCrossAlignment.center,
+                      children: [
+                        Icon(
+                          Icons.shopping_cart_checkout,
+                          color: Colors.white,
+                        ),
+                        SizedBox(width: 15),
+                        Text(
+                          '1',
+                          style: TextStyle(
+                            color: Colors.white,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                GestureDetector(
+                  onTap: () => payNow(),
+                  child: Container(
+                    height: 50,
+                    width: 120,
+                    decoration: const BoxDecoration(
+                      color: Colors.grey,
+                      borderRadius: BorderRadius.only(
+                        bottomRight: Radius.circular(5),
+                        topRight: Radius.circular(5),
+                      ),
+                    ),
+                    child: const Center(
+                      child: Text(
+                        'Order Now',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.w700,
+                        ),
+                      ),
+                    ),
+                  ),
+                )
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
